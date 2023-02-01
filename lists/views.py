494135bea92de2535227414b392dc05f190499d4
Suyash <suyash.d@netwebindia.com>
2023-02-01 23:18:50 +0530

    Add view function that renders apps's homepage

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,22 @@
+from __future__ import annotations
 from django.shortcuts import render
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def homepage(request: HttpRequest) -> HttpResponse:
+    """Renders the homepage of the application.
+
+    Parameters
+    ----------
+    request: An object representing HTTP request sent to web server.
+
+    Returns
+    -------
+    Object representing HTTP response containing HTML that can be used by
+    browser to render the homepage.
+    """
+    return render(request, "lists/index.html")
