ea123882465aeeb604b07c4b92835963a6a60bb2
Suyash <suyash.d@netwebindia.com>
2023-02-01 18:15:13 +0530

    Add comments explaining user story of user adding item to to-do list, saving and retrieving it at later time

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class HomePageTest(unittest.TestCase):
@@ -11,8 +12,36 @@ class HomePageTest(unittest.TestCase):
         self.browser.quit()
 
     def test_home_page(self):
-        self.browser.get("http://localhost:8000")
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
+        header = self.browser.find_element(By.ID, "h1")
+        self.assertIn("To-Do", header.text)
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish this test")
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
 
 
 if __name__ == "__main__":
