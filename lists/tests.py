fd13ca07e60b5b75dbab6058006916289666dfd7
Suyash <suyash.d@netwebindia.com>
2023-02-02 00:58:08 +0530

    Refactor homepage test to check whether correct HTML template was returned by response

--- lists/tests.py
+++ lists/tests.py
@@ -4,12 +4,6 @@ from django.test import TestCase
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_math_works(self):
+    def test_homepage(self):
         response = self.client.get("/")
-        self.assertIn(
-            "<title>To-Do lists</title>",
-            response.content.decode(),
-            "No title with 'To-Do lists' was found on homepage."
-        )
-        self.assertTrue(response.content.decode().strip().startswith("<html>"), "Response doesn't start with <html> tag.")
-        self.assertTrue(response.content.decode().strip().endswith("</html>"), "Response doesn't end with </html> tag.")
+        self.assertTemplateUsed(response, "lists/index.html")
