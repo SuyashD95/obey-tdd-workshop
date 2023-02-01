2881f6648d132eb7b5ab20f38f4f082085e414d3
Suyash <suyash.d@netwebindia.com>
2023-02-01 23:12:51 +0530

    Fix bugs in tests that was resulting in false negatives

--- lists/tests.py
+++ lists/tests.py
@@ -6,10 +6,10 @@ class HomePageTest(TestCase):
 
     def test_math_works(self):
         response = self.client.get("/")
-        self.assertHTMLEqual(
+        self.assertIn(
             "<title>To-Do lists</title>",
             response.content.decode(),
             "No title with 'To-Do lists' was found on homepage."
         )
         self.assertTrue(response.content.decode().startswith("<html>"), "Response doesn't start with <html> tag.")
-        self.assertTrue(response.content.decode().endswith("</html>"), "Response doesn't end with </html> tag.")
+        self.assertTrue(response.content.decode().endswith("</html>\n"), "Response doesn't end with </html> tag.")
