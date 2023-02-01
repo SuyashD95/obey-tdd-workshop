4ce5f8b8009435454a3ea0ec07fedbadbba3ebef
Suyash <suyash.d@netwebindia.com>
2023-02-02 00:47:08 +0530

    Strip whitespaces while testing for presence of <html></html> tags

--- lists/tests.py
+++ lists/tests.py
@@ -11,5 +11,5 @@ class HomePageTest(TestCase):
             response.content.decode(),
             "No title with 'To-Do lists' was found on homepage."
         )
-        self.assertTrue(response.content.decode().startswith("<html>"), "Response doesn't start with <html> tag.")
-        self.assertTrue(response.content.decode().endswith("</html>\n"), "Response doesn't end with </html> tag.")
+        self.assertTrue(response.content.decode().strip().startswith("<html>"), "Response doesn't start with <html> tag.")
+        self.assertTrue(response.content.decode().strip().endswith("</html>"), "Response doesn't end with </html> tag.")
