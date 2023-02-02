4af1c19cfb41f94a21b73fe6b4d3918337dabd13
Suyash <suyash.d@netwebindia.com>
2023-02-02 12:55:45 +0530

    Rename unit test & add meaningful error message

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,6 @@ from django.test import TestCase
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_homepage(self):
+    def test_uses_homepage_template(self):
         response = self.client.get("/")
-        self.assertTemplateUsed(response, "lists/index.html")
+        self.assertTemplateUsed(response, "lists/index.html", "Response doesn't return homepage template.")
