d4ca6f145ffe28557cfb293ef348581e4120a4ab
Suyash <suyash.d@netwebindia.com>
2023-02-02 13:02:11 +0530

    Add unit test that checks whether view function handles POST request correctly

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,7 @@ class HomePageTest(TestCase):
     def test_uses_homepage_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "lists/index.html", "Response doesn't return homepage template.")
+
+    def test_handles_post_requests(self):
+        response = self.client.post("/", {"item_text": "a list item"})
+        self.assertIn("a list item", response.content.decode(), "Homepage cannot handle POST requests")
